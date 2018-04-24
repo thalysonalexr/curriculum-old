4b6e136d71f57cd58018d6220ba06cb0d6ca8363
thalysonrodrigues <thalysonps3@gmail.com>
2018-04-24 03:20:39 -0400

    translate tabs to spaces

--- assets/js/navtopics.js
+++ assets/js/navtopics.js
@@ -2,22 +2,21 @@
 
 $(document).ready( function() {
 
-	function scrollPage(link, section) {
+  function scrollPage(link, section) {
+    $(link).click(function() {
+      $('html, body').animate({
+        scrollTop: $(section).offset().top
+      }, 600);
+    });
+  }
 
-        $(link).click(function() {
-            $('html, body').animate({
-                scrollTop: $(section).offset().top
-            }, 600);
-        });
-    }
-
-    scrollPage('#link-dados-pessoais',  '#dados-pessoais');
-    scrollPage('#link-contato',  '#contato');
-    scrollPage('#link-qualificacao',  '#qualificacao');
-    scrollPage('#link-info-compl',  '#info-compl');
-    scrollPage('#link-situacao',  '#situacao');
-    scrollPage('#link-formacao',  '#formacao');
-    scrollPage('#link-experiencia',  '#experiencia');
-    scrollPage('#link-cursos',  '#cursos');
-    scrollPage('#link-informatica',  '#informatica');
+  scrollPage('#link-dados-pessoais',  '#dados-pessoais');
+  scrollPage('#link-contato',  '#contato');
+  scrollPage('#link-qualificacao',  '#qualificacao');
+  scrollPage('#link-info-compl',  '#info-compl');
+  scrollPage('#link-situacao',  '#situacao');
+  scrollPage('#link-formacao',  '#formacao');
+  scrollPage('#link-experiencia',  '#experiencia');
+  scrollPage('#link-cursos',  '#cursos');
+  scrollPage('#link-informatica',  '#informatica');
 })
--- assets/js/toggle.js
+++ assets/js/toggle.js
@@ -2,14 +2,14 @@
 
 $(document).ready( function() {
 
-	$('#navbar').click( function() {
+  $('#navbar').click( function() {
 
-		if (!$('#navbarToggleExternalContent').is(':visible')) {
-			$(this).removeClass('collapsed');
-			$('#navbarToggleExternalContent').slideDown();
-		} else {
-			$('#navbarToggleExternalContent').slideUp();
-			$(this).addClass('collapsed');
-		}
-	});
+    if (!$('#navbarToggleExternalContent').is(':visible')) {
+      $(this).removeClass('collapsed');
+      $('#navbarToggleExternalContent').slideDown();
+    } else {
+      $('#navbarToggleExternalContent').slideUp();
+      $(this).addClass('collapsed');
+    }
+  });
 });
